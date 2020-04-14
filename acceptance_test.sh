5ff23128f1d1bf18a224adb3f0ca794e20940ed5
anumod-git <61692349+anumod-git@users.noreply.github.com>
2020-04-14 18:34:05 +0400

    Update acceptance_test.sh

--- acceptance_test.sh
+++ acceptance_test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 INTERNAL_HOST_IP=$(ip route show default | awk '/default/ {print $3}')
 echo ${INTERNAL_HOST_IP}                          
-test $(curl 172.17.0.1:8765/sum?a=1\&b=2)
+test $(curl 172.17.0.1:8765/sum?a=1\&b=2) -eq 3
 #test $(curl localhost:8765/sum?a=1\&b=2) -eq 3
 #echo "testing............"
