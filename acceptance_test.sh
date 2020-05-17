22449f84e3be542ff3677e67824c3c62cccee6d1
anumod-git <anumod.d.c@gmail.com>
2020-05-17 23:10:54 +0400

    Adding Redis

--- acceptance_test.sh
+++ acceptance_test.sh
@@ -6,5 +6,6 @@ echo $INTERNAL_HOST_IP
 #test $(curl $INTERNAL_HOST_IP) -eq 3
 #test $(curl 172.17.0.1:8765/sum?a=1\&b=2) -eq 3
 CALCULATOR_PORT=$(docker-compose port calculator 8080 | cut -d: -f2)
+echo $CALCULATOR_PORT 
 test $(curl 172.17.0.1:$CALCULATOR_PORT/sum?a=1\&b=2) -eq 3
 #echo "testing............"
