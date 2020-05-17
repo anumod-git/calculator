ac07c107d97c7a721c0a6fe85a84b86021b54810
anumod-git <anumod.d.c@gmail.com>
2020-05-17 21:55:36 +0400

    Adding Redis

--- acceptance_test.sh
+++ acceptance_test.sh
@@ -4,5 +4,7 @@ INTERNAL_HOST_IP=$(ip route show default | awk '/default/ {print $3}')
 #INTERNAL_HOST_IP=${INTERNAL_HOST_IP%$'\r'}
 echo $INTERNAL_HOST_IP                        
 #test $(curl $INTERNAL_HOST_IP) -eq 3
-test $(curl 172.17.0.1:8765/sum?a=1\&b=2) -eq 3
+#test $(curl 172.17.0.1:8765/sum?a=1\&b=2) -eq 3
+CALCULATOR_PORT=$(docker-compose port calculator 8080 | cut -d: -f2)
+test $(curl 172.17.0.1:$CALCULATOR_PORT/sum?a=1\&b=2) -eq 3
 #echo "testing............"
